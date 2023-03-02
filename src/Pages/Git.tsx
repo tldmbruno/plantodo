d97d0e9f669d094a46aad0b794b8559924aac21f
tldmbruno <dev.brunoperes@gmail.com>
2023-03-02 16:03:34 -0300

    Fix: Add Git links

--- src/Pages/Git.tsx
+++ src/Pages/Git.tsx
@@ -2,7 +2,8 @@ export default function About() {
 	return (
 		<>
 			<h1>Git</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, doloribus dolorum repellendus quos nisi impedit necessitatibus voluptate in! Aliquam vel, quaerat nisi placeat architecto inventore praesentium numquam incidunt quae modi.</p>
+			<a href='https://github.com/tldmbruno/plantodo'>Get the Source code</a>
+			<a href='https://github.com/tldmbruno'>Follow Bruno Peres on Github</a>
 		</>
 	);
 }
