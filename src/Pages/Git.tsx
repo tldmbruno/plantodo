0c043fb4046d8453bcb37d654428da2f3f3d2349
tldmbruno <dev.brunoperes@gmail.com>
2023-03-03 21:44:07 -0300

    Fix: Change Git links from <a> to <button>

--- src/Pages/Git.tsx
+++ src/Pages/Git.tsx
@@ -2,8 +2,18 @@ export default function About() {
 	return (
 		<>
 			<h1>Git</h1>
-			<a href='https://github.com/tldmbruno/plantodo'>Get the Source code</a>
-			<a href='https://github.com/tldmbruno'>Follow Bruno Peres on Github</a>
+			<div className='flex'>
+				<div className='flex s-gap'>
+					<button onClick={() => window.location.href = 'https://github.com/tldmbruno/plantodo'}>
+						Get the Source code
+					</button>
+					<button onClick={() => window.location.href = 'https://github.com/tldmbruno'}>
+						Follow Bruno Peres on Github
+					</button>
+				</div>
+
+				<span></span>
+			</div>
 		</>
 	);
 }
