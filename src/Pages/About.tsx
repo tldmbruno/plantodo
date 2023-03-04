e85c2edafc0feb4c40b79f32f07e07ba76f02a7c
tldmbruno <dev.brunoperes@gmail.com>
2023-03-03 21:43:36 -0300

    Feat: Add secret drawing canvas

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,8 +1,14 @@
+import '../PaintCanvas/PaintCanvas'
+import PaintCanvas from '../PaintCanvas/PaintCanvas';
+
 export default function About() {
 	return (
 		<>
 			<h1>About</h1>
-			<p>PlanTODO is a simple yet useful note taking web application. It's unusual features, however, allow the user to express themselves better with additional functionality. Those tools are currently under development.</p>
+			<p>PlanTODO is a simple yet useful note taking web application. It's unusual features, however, will allow the user to express themselves better with additional functionality. Those tools are currently under development.</p>
+			<p>To compensate, I will allow you to make a drawing below if you want. Give it a try.</p>
+
+			<PaintCanvas />
 		</>
 	);
 }
