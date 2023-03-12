cf792731baf57adbc2a91c7a5356a79f731522a1
tldmbruno <dev.brunoperes@gmail.com>
2023-03-12 12:48:58 -0300

    Feat: Add Back Button

---
+++ src/ButtonBack/ButtonBack.tsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+export default function ButtonBack() {
+	return (
+		<Link to='..'>Go back</Link>
+	)
+}
--- src/Pages/Edit.tsx
+++ src/Pages/Edit.tsx
@@ -1,9 +1,11 @@
+import ButtonBack from "../ButtonBack/ButtonBack";
 import TodoList from "../TodoList/TodoList"
 import TodoListTitle from "../TodoListTitle/TodoListTitle"
 
 export default function Edit() {
 	return (
 		<>
+			<ButtonBack />
 			<TodoListTitle />
 			<TodoList />
 		</>
