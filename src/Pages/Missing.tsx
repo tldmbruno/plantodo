814585e42fc2f400fbcf571452dde037078c4b14
tldmbruno <dev.brunoperes@gmail.com>
2023-03-15 14:47:53 -0300

    Fix: Change 404 message

--- src/Pages/Missing.tsx
+++ src/Pages/Missing.tsx
@@ -2,7 +2,7 @@ export default function About() {
 	return (
 		<>
 			<h1>404 - Page Not Found</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, doloribus dolorum repellendus quos nisi impedit necessitatibus voluptate in! Aliquam vel, quaerat nisi placeat architecto inventore praesentium numquam incidunt quae modi.</p>
+			<p>Your todo list is in another castle.</p>
 		</>
 	);
 }
