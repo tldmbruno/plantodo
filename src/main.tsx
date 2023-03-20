f6594656b2ca022dcb84170cfdeaabb58a301428
tldmbruno <dev.brunoperes@gmail.com>
2023-03-20 16:26:27 -0300

    Fix: Stretch NavBar and apply line weight consistency

--- src/main.tsx
+++ src/main.tsx
@@ -13,21 +13,22 @@ import EditPage from './Pages/Edit';
 import AboutPage from './Pages/About';
 import GitPage from './Pages/Git';
 import MissingPage from './Pages/Missing';
+import WelcomePage from './Pages/Welcome';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
 
+      <NavBar />
+      
       <div className='container'>
-
-        <NavBar />
-
         <main>
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/edit/*' element={<EditPage />} />
             <Route path='/about' element={<AboutPage />} />
             <Route path='/git' element={<GitPage />} />
+            <Route path='/welcome' element={<WelcomePage />} />
             <Route path='*' element={<MissingPage />}/>
           </Routes>
         </main>
