76fc99aa1297a6fa30bb3e39cee7d8f08951db9a
tldmbruno <dev.brunoperes@gmail.com>
2023-03-20 16:28:52 -0300

    [WIP] Feat: Add Welcome Page, accessible through clicking the logo

---
+++ src/Pages/Welcome.tsx
@@ -0,0 +1,26 @@
+import Divider from "../Divider/Divider";
+
+export default function Welcome() {
+	return (
+		<>
+			<h1>PlanTODO</h1>
+			
+			<Divider />
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, laboriosam. Illum, eligendi nesciunt. Modi inventore beatae asperiores consequatur aliquid, rem sunt quia quae et. Similique ullam sit beatae soluta iusto! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ex accusamus rem vel, provident dolorum iste voluptas maiores odit consectetur assumenda aut nesciunt suscipit magnam voluptatibus debitis? Sapiente, libero velit.</p>
+			<button>Start now</button>
+
+			<Divider />
+
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates enim asperiores labore laudantium accusantium suscipit, repudiandae iste optio nesciunt, assumenda excepturi, nostrum aliquid voluptas libero repellendus itaque! Accusantium, architecto aliquam.</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nobis dolore maiores voluptatibus necessitatibus neque! Error dicta ducimus recusandae, praesentium odio quod earum animi vel voluptate accusamus ea porro maxime!</p>
+
+			<Divider />
+
+			<h2>Git</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus reiciendis cupiditate modi tempore et facere itaque praesentium! Vel quis dicta molestias hic, doloremque ab sint, et mollitia cumque, placeat itaque!</p>
+			<button>Git source code</button>
+			<button>Follow Bruno Peres</button>
+		</>
+	);
+}
