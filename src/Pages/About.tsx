e3c394aec77cdccbc320d38ef7a4231a92bf972d
tldmbruno <dev.brunoperes@gmail.com>
2023-03-20 16:27:46 -0300

    Fix (AboutPage): Add interactive logo

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,4 +1,5 @@
 import Divider from '../Divider/Divider';
+import Logo from '../Logo/Logo';
 import '../PaintCanvas/PaintCanvas'
 import PaintCanvas from '../PaintCanvas/PaintCanvas';
 
@@ -6,8 +7,10 @@ export default function About() {
 	return (
 		<>
 			<h1>About</h1>
+			
 			<Divider />
-			<p>PlanTODO is a simple yet useful note taking web application. It's unusual features, however, will allow the user to express themselves better with additional functionality. Those tools are currently under development.</p>
+
+			<p><Logo /> is a simple yet useful note taking web application. It's unusual features, however, will allow the user to express themselves better with additional functionality. Those tools are currently under development.</p>
 			<p>To compensate, I will allow you to make a drawing below if you want. Give it a try.</p>
 
 			<PaintCanvas />
