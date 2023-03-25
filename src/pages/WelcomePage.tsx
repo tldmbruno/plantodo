7ded95b906e954177f5ce3694eda6c401d188c0a
tldmbruno <dev.brunoperes@gmail.com>
2023-03-25 00:54:34 -0300

    Fix (Welcome Page): Add dividers to cards, tweak font sizes

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -22,16 +22,19 @@ export default function WelcomePage() {
 				<div className='flex gap'>
 					<div className='card'>
 						<h2>🚀 Fast and easy to use</h2>
+						<Divider />
 						<p>PlanTODO prioritizes speed and usability. This means that the app should open quickly and be intuitive to use, allowing users to quickly add and manage their tasks without getting bogged down by complicated menus or features.</p>
 					</div>
 
 					<div className='card'>
 						<h2>🎨 Simple design</h2>
+						<Divider />
 						<p>PlanTODO aims to keep things simple by avoiding clutter and unnecessary features. The goal is to create an app that is easy to use and navigate, without overwhelming the user with too many options.</p>
 					</div>
 
 					<div className='card'>
 						<h2>🎲 Randomization</h2>
+						<Divider />
 						<p>This is a unique feature of PlanTODO that sets it apart from other todonotes apps. The randomization feature allows users to randomly select a task from their list, helping them prioritize their to-do list and avoid getting stuck on one task for too long.</p>
 					</div>
 				</div>
@@ -44,22 +47,27 @@ export default function WelcomePage() {
 				<div className='grid gap'>
 					<div className='card'>
 						<h2>1. Create a new list</h2>
+						<Divider />
 						<p>Insert the name and press Enter or click on the button "Create New List".</p>
 					</div>
 					<div className='card'>
 						<h2>2. Click the name to access the list</h2>
+						<Divider />
 						<p>Your new list should appear under the input bar.</p>
 					</div>
 					<div className='card'>
 						<h2>3. Create a new item</h2>
+						<Divider />
 						<p>Write some text, "Study for my exam" for example, then press Enter or click on the button "New Item".</p>
 					</div>
 					<div className='card'>
 						<h2>4. Add some more</h2>
+						<Divider />
 						<p>Repeat the third step to add more items to your list. You can mark the tasks as done by clicking the checkbox on the left of the task's name.</p>
 					</div>
 					<div className='card'>
 						<h2>5. Not sure what to do next?</h2>
+						<Divider />
 						<p>Click the randomizer button, on the top right corner.</p>
 					</div>
 				</div>
