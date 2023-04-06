51fad17676f02aeda9dcd7a8594d20a338f301cc
Bruno Peres <dev.brunoperes@gmail.com>
2023-04-06 19:42:13 -0300

    Fix: Adjust ListSelector link boundaries

--- src/components/ListSelector/ListSelector.tsx
+++ src/components/ListSelector/ListSelector.tsx
@@ -5,150 +5,148 @@ import { loadData, saveData } from "../DataHandler/DataHandler";
 import Divider from "../Divider/Divider";
 import { InputItem } from "../ItemInput/ItemInput";
 
-import './ListSelector.css';
-
 interface ListData {
-	fetchId: number;
-	title: string;
-	lastModification: string;
+  fetchId: number;
+  title: string;
+  lastModification: string;
 }
 
 export function updateModificationDate(fetchId: number): void {
-	const listsData = loadData<ListData[]>('selectorData') ?? [];
-	const listsDataIndex = listsData.findIndex((i) => i.fetchId === fetchId);
+  const listsData = loadData<ListData[]>('selectorData') ?? [];
+  const listsDataIndex = listsData.findIndex((i) => i.fetchId === fetchId);
 
-	listsData[listsDataIndex].lastModification = getFormattedModificationDate();
+  listsData[listsDataIndex].lastModification = getFormattedModificationDate();
 
-	saveData(listsData, 'selectorData');
+  saveData(listsData, 'selectorData');
 }
 
 function getFormattedModificationDate(): string {
-	const currentDate = new Date();
-	return currentDate.toLocaleString();
+  const currentDate = new Date();
+  return currentDate.toLocaleString();
 }
 
 export default function ListSelector() {
-	// Declares and loads state data
-	const itemRef = useRef<HTMLInputElement>(null);
-	const [ listsData, setListsData ] = useState(() => {
-		const data = loadData<ListData[]>('selectorData');
-		return data ? data : [];
-	});
+  // Declares and loads state data
+  const itemRef = useRef<HTMLInputElement>(null);
+  const [ listsData, setListsData ] = useState(() => {
+    const data = loadData<ListData[]>('selectorData');
+    return data ? data : [];
+  });
 
-	// Deletion state management
-	const [ askingForDeletion, setAskingForDeletion ] = useState(false);
-	const [ listIndexForDeletion, setListIndexForDeletion ] = useState(-1);
-	
-	// Auto save on every change
-	useEffect(() => {
-		saveData(listsData, 'selectorData');
-	},[listsData])
-
-	// Creates a new list, defaulting to 'Unnammed list'
-	function createList(): void {
-		// Gets the new list's title
-		let newTitle = itemRef.current?.value.trim() ?? '';
-		newTitle = newTitle.length === 0 ? 'Unnammed List' : newTitle;
-
-		// Verify if there's already a list with the same name
-		let occourences = 0;
-		listsData.forEach(list => {
-			if (list.title == newTitle ||
-					list.title == `${newTitle} (${occourences})`) {
-				occourences++;
-			}
-		});
-
-		// If so, renames it appropriately
-		if (occourences > 0) {
-			newTitle = `${newTitle} (${occourences})`;
-		}
-
-		// Creates the new list
-		const newList: ListData = {
-			fetchId: Date.now(),
-			title: newTitle,
-			lastModification: getFormattedModificationDate()
-		};
-
-		// Adds to the state array
-		setListsData([...listsData, newList]);
-	}
-
-	function onDeleteRequest(list: ListData): JSX.Element | null {
-		const listsDataIndex = listsData.findIndex((i) => i.fetchId === list.fetchId);
-
-		// Checks if the index is valid before asking for confirmation
-		if (listsDataIndex !== -1) {
-			// Evokes popUp for confirmation
-			setAskingForDeletion(true);
-			setListIndexForDeletion(listsDataIndex);
-		}
-
-		return null;
-	}
-
-	// Deletes the list and also the content in it from Local Storage
-	function deleteList(list: ListData): void {
-		const listsDataIndex = listsData.findIndex((i) => i.fetchId === list.fetchId);
-		const todoListFileData = 'todoListData' + listsData[listsDataIndex].fetchId;
+  // Deletion state management
+  const [ askingForDeletion, setAskingForDeletion ] = useState(false);
+  const [ listIndexForDeletion, setListIndexForDeletion ] = useState(-1);
   
-		// Checks if the index is valid before deleting anything
+  // Auto save on every change
+  useEffect(() => {
+    saveData(listsData, 'selectorData');
+  },[listsData])
+
+  // Creates a new list, defaulting to 'Unnammed list'
+  function createList(): void {
+    // Gets the new list's title
+    let newTitle = itemRef.current?.value.trim() ?? '';
+    newTitle = newTitle.length === 0 ? 'Unnammed List' : newTitle;
+
+    // Verify if there's already a list with the same name
+    let occourences = 0;
+    listsData.forEach(list => {
+      if (list.title == newTitle ||
+          list.title == `${newTitle} (${occourences})`) {
+        occourences++;
+      }
+    });
+
+    // If so, renames it appropriately
+    if (occourences > 0) {
+      newTitle = `${newTitle} (${occourences})`;
+    }
+
+    // Creates the new list
+    const newList: ListData = {
+      fetchId: Date.now(),
+      title: newTitle,
+      lastModification: getFormattedModificationDate()
+    };
+
+    // Adds to the state array
+    setListsData([...listsData, newList]);
+  }
+
+  function onDeleteRequest(list: ListData): JSX.Element | null {
+    const listsDataIndex = listsData.findIndex((i) => i.fetchId === list.fetchId);
+
+    // Checks if the index is valid before asking for confirmation
     if (listsDataIndex !== -1) {
-			// Delete the list from the array
+      // Evokes popUp for confirmation
+      setAskingForDeletion(true);
+      setListIndexForDeletion(listsDataIndex);
+    }
+
+    return null;
+  }
+
+  // Deletes the list and also the content in it from Local Storage
+  function deleteList(list: ListData): void {
+    const listsDataIndex = listsData.findIndex((i) => i.fetchId === list.fetchId);
+    const todoListFileData = 'todoListData' + listsData[listsDataIndex].fetchId;
+  
+    // Checks if the index is valid before deleting anything
+    if (listsDataIndex !== -1) {
+      // Delete the list from the array
       const newList = [...listsData];
       newList.splice(listsDataIndex, 1);
 
-			// Erase the content from Local Storage
-			localStorage.removeItem(todoListFileData);
+      // Erase the content from Local Storage
+      localStorage.removeItem(todoListFileData);
 
-			// Reset the index for deletion
-			setListIndexForDeletion(-1);
+      // Reset the index for deletion
+      setListIndexForDeletion(-1);
   
-			// Apply the modifications
+      // Apply the modifications
       setListsData(newList);
     }
-	}
+  }
 
-	return (
-		<>
-			<h1 className='title'>Select or create your list</h1>
+  return (
+    <>
+      <h1 className='title'>Select or create your list</h1>
 
-			<div className='flex gap'>
-				<InputItem
-					buttonText={'Create new list'}
-					itemRef={itemRef}
-					submitFunction={createList}/>
-			</div>
+      <div className='flex gap'>
+        <InputItem
+          buttonText={'Create new list'}
+          itemRef={itemRef}
+          submitFunction={createList}/>
+      </div>
 
-			<Divider />
+      <Divider />
 
-			<ul className='list'>
-				{listsData.map(list =>
-					<li key={list.fetchId}>
-						<Link className='fullWidth flex' to={'/edit/' + list.title} state={list}>
-							<div>
-								<span className='fileName breakableWord'>{list.title}</span>
-							</div>
-							<div>
-								<span className='mini optional'>Last modified {list.lastModification}</span>
-								<button className='danger' onClick={() => onDeleteRequest(list)}>Delete</button>
-							</div>
-						</Link>
-					</li>
-				)}
-			</ul>
-			
-			{listIndexForDeletion != -1 ?
-			<ConfirmationPopUp
+      <ul className='list'>
+        {listsData.map(list =>
+          <li key={list.fetchId}>
+            <Link className='fullWidth flex' to={'/edit/' + list.title} state={list}>
+              <div>
+                <span className='breakableWord'>{list.title}</span>
+              </div>
+            </Link>
+            <div>
+                <span className='mini optional'>{list.lastModification}</span>
+                <button className='danger' onClick={() => onDeleteRequest(list)}>Delete</button>
+            </div>
+          </li>
+        )}
+      </ul>
+      
+      {listIndexForDeletion != -1 ?
+      <ConfirmationPopUp
         title={`Delete "${listsData[listIndexForDeletion].title}"?`}
         description={`This action cannot be undone.`}
-				visible={askingForDeletion}
-				setVisible={setAskingForDeletion}
-				onConfirm={() => {deleteList(listsData[listIndexForDeletion])}}
-				confirmLabel={'Delete'}
-				dangerousConfirm={true}/>
-			: <></>}
-		</>
-	);
+        visible={askingForDeletion}
+        setVisible={setAskingForDeletion}
+        onConfirm={() => {deleteList(listsData[listIndexForDeletion])}}
+        confirmLabel={'Delete'}
+        dangerousConfirm={true}/>
+      : <></>}
+    </>
+  );
 }
