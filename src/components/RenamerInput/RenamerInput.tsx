7ceb46524b66e3517dc455a53ae1d7e1436aadf0
Bruno Peres <dev.brunoperes@gmail.com>
2023-04-20 19:28:03 -0300

    Fix (RenamerInput): Remove ListData import, remove unnesessary boolean 'visible', remove 'hidden' property.

--- src/components/RenamerInput/RenamerInput.tsx
+++ src/components/RenamerInput/RenamerInput.tsx
@@ -1,25 +1,22 @@
 import { FormEvent, useRef } from "react";
-import { ListData } from "../TodoList/TodoList";
 
 interface RenamerInputProps {
   formId?: string;
   listId: number;
   currentTitle: string;
   setTitle: (listId: number, newTitle: string) => void;
-  visible?: boolean;
 }
 
-export default function RenamerInput({formId = Math.random().toString(), listId, currentTitle, setTitle, visible = false}: RenamerInputProps) {
+export default function RenamerInput({formId = Math.random().toString(), listId, currentTitle, setTitle}: RenamerInputProps) {
   const titleRef = useRef<HTMLInputElement>(null);
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
     setTitle(listId, titleRef?.current?.value ?? '');
-    visible = false;
   }
 
   return (
-    <form id={formId} method='POST' onSubmit={handleSubmit} hidden={!visible} className='flex noPadding'>
+    <form id={formId} method='POST' onSubmit={handleSubmit} className='flex noPadding'>
       <input className='renameInput' type='text' ref={titleRef} defaultValue={currentTitle} autoFocus onBlur={handleSubmit}/>
     </form>
   );
