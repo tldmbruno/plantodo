85ad1230984b31201a3613d53a2a200e08d63f53
Bruno Peres <dev.brunoperes@gmail.com>
2023-04-21 20:21:49 -0300

    Fix: Remove unused EditPage import

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import NavBar from './components/NavBar/NavBar';
 
 import WelcomePage from './pages/WelcomePage';
 import CreatePage from './pages/CreatePage';
-import EditPage from './pages/EditPage';
 import MissingPage from './pages/MissingPage';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
