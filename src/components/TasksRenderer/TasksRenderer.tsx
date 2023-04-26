433fb3352329f36dae25c5517c4f7359ddb1bb74
Bruno Peres <dev.brunoperes@gmail.com>
2023-04-26 16:37:26 -0300

    Feat (TasksRenderer): Add tutorial.

--- src/components/TasksRenderer/TasksRenderer.tsx
+++ src/components/TasksRenderer/TasksRenderer.tsx
@@ -11,22 +11,31 @@ interface PropsRenderList {
 // Shows all items in a particular list
 export default function TasksRenderer({toggleDone, moveTask, editTask, deleteTask, tasks}: PropsRenderList) {
   return (
-    <ul className='list'>
-      {tasks.map((task, index) =>
-      <li key={task.id} className={task.done ? 'done' : ''}>
-        <div className='fullWidth' onClick={() => toggleDone(task)}>
-          <input title={`${task.done?'Unmark':'Mark'} ${task.text} as done`} type='checkbox' checked={task.done} onChange={() => null}/>
-          <label>{task.text}</label>
-        </div>
-        <div className='visibleOnParentHover'>
-          <button title='Move upwards' className='optional' onClick={() => moveTask(task, -1)} hidden={index==0}>⬆</button>
-          <button title='Move downwards' className='optional' onClick={() => moveTask(task, +1)} hidden={index+1 == tasks.length}>⬇</button>
+    <>
+      {tasks.length > 0 ?
+        <ul className='list'>
+          {tasks.map((task, index) =>
+          <li key={task.id} className={task.done ? 'done' : ''}>
+            <div className='fullWidth' onClick={() => toggleDone(task)}>
+              <input title={`${task.done?'Unmark':'Mark'} ${task.text} as done`} type='checkbox' checked={task.done} onChange={() => null}/>
+              <label>{task.text}</label>
+            </div>
+            <div className='visibleOnParentHover'>
+              <button title='Move upwards' className='optional' onClick={() => moveTask(task, -1)} hidden={index==0}>⬆</button>
+              <button title='Move downwards' className='optional' onClick={() => moveTask(task, +1)} hidden={index+1 == tasks.length}>⬇</button>
 
-          <button title={`Rename ${task.text}`} onClick={() => editTask(task)}>📝</button>
-          <button title={`Delete ${task.text}`} className='danger' onClick={() => deleteTask(task)}>❌</button>
-        </div>
-      </li>
-      )}
-    </ul>
+              <button title={`Rename ${task.text}`} onClick={() => editTask(task)}>📝</button>
+              <button title={`Delete ${task.text}`} className='danger' onClick={() => deleteTask(task)}>❌</button>
+            </div>
+          </li>
+          )}
+        </ul>
+      :
+      <div className='container fade-in'>
+        <span className='optional'>💡 Now create your first task by clicking the 'Add' button!</span>
+        <span className='mobile'>💡 Now create your first task by clicking the ➕ button!</span>
+      </div>
+      }
+    </>
   );
 }
