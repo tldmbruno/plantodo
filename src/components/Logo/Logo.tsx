e996287d83df303bde370588dfb4ed8b22cf63bb
Bruno Peres <dev.brunoperes@gmail.com>
2023-05-01 17:23:40 -0300

    Refactor: Change Logo.css to Logo.module.css

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import './Logo.css';
+import style from './Logo.module.css';
 
 export default function Logo() {
 	return (
-		<div title='Go to home page' className='logo slide-in'>
+		<div title='Go to home page' className={style.logo + ' slide-in'}>
 			<Link to='/'>PlanTODO</Link>
 		</div>
 	);
