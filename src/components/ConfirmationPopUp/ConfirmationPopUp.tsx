6edc1ee5aba159dce1a818ec57918143612d4cdc
Bruno Peres <dev.brunoperes@gmail.com>
2023-05-02 21:53:45 -0300

    Refactor: Change ConfirmationPopUp.css to module.css

--- src/components/ConfirmationPopUp/ConfirmationPopUp.tsx
+++ src/components/ConfirmationPopUp/ConfirmationPopUp.tsx
@@ -1,6 +1,6 @@
 import Divider from "../Divider/Divider";
 
-import './ConfirmationPopUp.css';
+import style from './ConfirmationPopUp.module.css';
 
 interface PropsConfirmationPopUp {
 	title?: string;
@@ -31,12 +31,12 @@ export default function ConfirmationPopUp(
 	return (
 		visible ? <>
 			<div className='popUp' onClick={(e) => {setVisible(false); e.stopPropagation()}}>
-				<div className='popUpInner notTooWide' onClick={(e) => e.stopPropagation()}>
+				<div className={style.popUpInner + ' notTooWide'} onClick={(e) => e.stopPropagation()}>
 					<h2 className='breakableWord'>{title}</h2>
 					<Divider />
 					<p className='centered'>{description}</p>
 					<div className='flex gap'>
-						<button className={`${dangerousConfirm? 'danger' : ''} fullWidth notTooWide`}
+						<button className={style.confirmationButton + ` ${dangerousConfirm? 'danger' : ''} fullWidth`}
 							onClick={() => handleConfirm()}>{confirmLabel}</button>
 						<button className='fullWidth' onClick={() => setVisible(false)}>Cancel</button>
 					</div>
